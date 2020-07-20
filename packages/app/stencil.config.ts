7baceb984ea7fd633140e582a40a83bffef40514
bitflower <matthias.max@bitflower.net>
2020-07-20 17:14:04 +0200

    plugins

--- packages/app/stencil.config.ts
+++ packages/app/stencil.config.ts
@@ -1,4 +1,5 @@
 import { Config } from '@stencil/core';
+import builtins from 'rollup-plugin-node-builtins';
 
 // https://stenciljs.com/docs/config
 
@@ -11,7 +12,8 @@ export const config: Config = {
       type: 'www',
       // comment the following line to disable service workers in production
       serviceWorker: null,
-      baseUrl: 'https://myapp.local/'
-    }
-  ]
+      baseUrl: 'https://myapp.local/',
+    },
+  ],
+  plugins: [builtins()],
 };
